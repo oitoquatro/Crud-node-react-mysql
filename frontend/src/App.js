5887e6d5752015824125f20f795313281c54ab21
PJPJunior <paulojpjunior@gmail.com>
2023-04-25 21:51:57 -0300

    Criação dos componentes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,33 @@
 import GlobalStyle from "./styles/global";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import styled from "styled-components";
+import Form from "./components/Form";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``;
 
 function App() {
 
   return (
-    <> 
+    <>
+      <Container>
+        <Title>USUÁRIOS</Title>
+        <Form />
+      </Container>
       <GlobalStyle />
       <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
-    </> 
-  ); 
+    </>
+  );
 }
 
 export default App;
