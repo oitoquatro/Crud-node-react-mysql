7555185794462f55fae24ce1e95d727b04004229
PJPJunior <paulojpjunior@gmail.com>
2023-05-02 21:47:12 -0300

    correção

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -11,7 +11,7 @@ export const getUsers = (_, res) => {
 };
 
 export const addUser = (req, res) => {
-  const q = "INSERT INTO usuarios(`nome`, `email`, `fone`, `data_nascimento`) VALUES(?)";
+  const q = "INSERT INTO new_table(`nome`, `email`, `fone`, `data_nascimento`) VALUES(?)";
 // Insert into serve para adicionar o dado na tabela
   const values = [
     req.body.nome,
@@ -29,7 +29,7 @@ export const addUser = (req, res) => {
 
 export const updateUser = (req, res) => {
   const q =
-  "UPDATE usuarios SET `nome` = ?, `email` = ?, `fone` = ?, `data_nascimento` = ? WHERE `id` = ?";
+  "UPDATE new_table SET `nome` = ?, `email` = ?, `fone` = ?, `data_nascimento` = ? WHERE `id` = ?";
 
   const values = [
     req.body.nome,
@@ -46,7 +46,7 @@ export const updateUser = (req, res) => {
 };
 
 export const deleteUser = (req, res) => {
-  const q = "DELETE FROM usuarios WHERE `id` = ?";
+  const q = "DELETE FROM new_table WHERE `id` = ?";
 
   db.query(q, [req.params.id], (err) => {
     if (err) return res.json(err);
